d26ce4d7934d9249cdaa00839379c872948d669d
AlonViglez <jesusalonsovilla2002@outlook.com>
2022-05-02 20:46:30 -0500

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $database = "userOrigenDeLosMemes";
 $user = "root";
-$password = "Maullino1011";
+$password = "";
 
 try {
   $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
