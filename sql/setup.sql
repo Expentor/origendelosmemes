16a222a339529aba93586764fdca9a3f2371d597
AlonViglez <jesusalonsovilla2002@outlook.com>
2022-05-02 20:43:39 -0500

    Update setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -10,29 +10,40 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255),
-    confirm_password VARCHAR(255),
-    password VARCHAR(255),
-    isAdmin TINYINT
+    password VARCHAR(255)
 );
+
+CREATE TABLE admins (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    isAdmin Boolean
+);
+
+DROP TABLE IF EXISTS articles;
 
 CREATE TABLE articles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100),
-    information TEXT
-    links
-    DATE
-    author
-    img
+    subtitle VARCHAR(50),
+    publish_date DATE,
+    author VARCHAR(50),
+    picture VARCHAR(100),
+    origin VARCHAR(50),
+    information TEXT,
+    likes INT,
+    dislikes INT,
+    links VARCHAR(255),
+    isPublished BOOLEAN
 );
 
-CREATE TABLE image (
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    filename VARCHAR(100)
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comments VARCHAR(255),
+    reply VARCHAR(255)
 );
-    -- email VARCHAR(255),
-    -- password VARCHAR(255),
-    -- isAdmin TINYINT
 
-
-INSERT INTO users (username, email, password) VALUES ("Adair", "adair@adair.com", "hola123", "1");
-
+INSERT INTO users (username, email, password) VALUES ("Adair", "adair@adair.com", "hola123");
+INSERT INTO admins (username, email, password, isAdmin) VALUES ("Adair", "adair@gmail.com", "vbnfgh213", True);
+INSERT INTO articles (publish_date, title, information, links) VALUES ("2022-04-27", "Jinx the cat", "jinx is a cat with big green eyes", "https://twitter.com/bigfootjinx");
