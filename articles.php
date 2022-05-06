3121327b22c9ef46059c42d193edc14647c5f640
AlonViglez <jesusalonsovilla2002@outlook.com>
2022-05-06 09:23:02 -0500

    Update articles.php

--- articles.php
+++ articles.php
@@ -26,7 +26,6 @@ require "database.php";
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="articlescss.css">
-    <link rel="stylesheet" href="styles.css">
 	<title>PI Work</title>
 </head>
 <body>
@@ -108,5 +107,6 @@ require "database.php";
 		</div>
 
 	<script src="app.js"></script>
+  <script src="script.js"></script>
     
   <?php require "partials/footer.php" ?>
